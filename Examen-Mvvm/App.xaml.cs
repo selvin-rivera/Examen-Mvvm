87c9f21cacc87c65abb28b0978c51f702270087f
selvin-rivera <nrivera.clements@gmail.com>
2024-06-19 21:15:28 -0600

    Add project files.

---
+++ Examen-Mvvm/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Examen_Mvvm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Examen-Mvvm/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Examen_Mvvm
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
